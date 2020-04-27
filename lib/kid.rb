64d6d890403202db9f086ed1e5dfb5a3268604d0
Dina Kendle <dina.kendle@gmail.com>
2020-04-27 01:58:05 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,16 +2,14 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
-class Kid 
- extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
-  include Dance 
+class Kid
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
   
-  attr_accessor :name 
   
-  # def initialize(name)
-  #   @name = name 
-  # end
-  
-  # extend MetaDancing
 end
