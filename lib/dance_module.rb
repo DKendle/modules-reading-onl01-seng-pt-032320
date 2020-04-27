3a70b1d64992bf858f875f13582c941badba1e2f
Dina Kendle <dina.kendle@gmail.com>
2020-04-27 02:00:13 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -9,8 +9,8 @@ module Dance
   end 
   
   def pirouette
-    "I'm dong a pirouette"
-  end 
+      "I'm doing a pirouette"
+    end
   
   def take_a_bow
     "Thank you, thank you. It was a pleasure to dance for you all."
